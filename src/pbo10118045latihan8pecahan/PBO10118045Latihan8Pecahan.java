2ae203b752a951d4f7757b613033e61271d1286a
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:52:48 +0700

    first commit

---
+++ src/pbo10118045latihan8pecahan/PBO10118045Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan8pecahan;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program :Pecahan
+ */
+public class PBO10118045Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " +d);
+    }
+    
+}
